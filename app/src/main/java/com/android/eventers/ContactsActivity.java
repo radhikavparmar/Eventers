724cff978353faa3c1847b836fd89d747fb8cf6f
rparmar <radhika.vparmar@gmail.com>
2017-06-26 17:05:02 +0530

    remove data duplication

--- app/src/main/java/com/android/eventers/ContactsActivity.java
+++ app/src/main/java/com/android/eventers/ContactsActivity.java
@@ -38,18 +38,20 @@ public class ContactsActivity extends AppCompatActivity implements ContactsAdapt
     Contacts contactsObject;
     TextView noItem;
     private ArrayList<Contacts> contactsArrayList;
+    ArrayList<String> tempList;
     private Parcelable mListState;
     private LinearLayoutManager mLayoutManager;
     SharedPreferences mSharedPreferences;
     SharedPreferences.Editor mEditor;
+    private boolean mCalledFromOncreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
-
-        noItem = (TextView)findViewById(R.id.no_listitem_in_contacts);
+        mCalledFromOncreate = true;
+        noItem = (TextView) findViewById(R.id.no_listitem_in_contacts);
         noItem.setVisibility(View.GONE);
         mFloatingActionButton = (FloatingActionButton) findViewById(R.id.add_fab_in_main);
         contactsArrayList = new ArrayList<Contacts>();
@@ -79,42 +81,45 @@ public class ContactsActivity extends AppCompatActivity implements ContactsAdapt
                         phoneNumberStr = phoneNumberStr.replaceAll(" ", "");
 
                         if (phoneNumberStr.contains(mobileNumber)) {
+
                         } else {
                             mobileNumber = String.valueOf(phoneNumber.getNationalNumber());
-                               Log.e("phone: ", " " + phoneNumber);
-                            contactsObject.setMobileNumber(mobileNumber);
-
+                            if(!tempList.contains(mobileNumber)) {
+                                Log.e("phone: ", " " + phoneNumber);
+                                contactsObject.setMobileNumber(mobileNumber);
+                                tempList.add(mobileNumber);
+                            }
                         }
 
                     } else {
 
-                        if (contactsObject!=null) {
+                        if (contactsObject != null) {
                             contactsArrayList.add(contactsObject);
                         }
                         contactsObject = new Contacts();
-
+                        tempList = new ArrayList<String>();
                         contactName = name;
                         mobileNumber = String.valueOf(phoneNumber.getNationalNumber());
-                         Log.e("name: ", " " + name);
-                          Log.e("phone: ", " " + mobileNumber);
+                        tempList.add(mobileNumber);
+                        Log.e("name: ", " " + name);
+                        Log.e("phone: ", " " + mobileNumber);
 
                         contactsObject.setName(name);
                         contactsObject.setMobileNumber(mobileNumber);
-                        contactsObject.setFlag(mSharedPreferences.getBoolean("checkbox_"+name,false));
-                        contactsObject.setSelectedMobileNumber(mSharedPreferences.getString("selected_mobile_number_for_"+name,mobileNumber));
+                        contactsObject.setFlag(mSharedPreferences.getBoolean("checkbox_" + name, false));
+                        contactsObject.setSelectedMobileNumber(mSharedPreferences.getString("selected_mobile_number_for_" + name, mobileNumber));
                     }
 
                 }
             } catch (Exception e) {
             }
-            if(phones.isLast()){
+            if (phones.isLast()) {
                 contactsArrayList.add(contactsObject);
             }
 
         }
         phones.close();
-
-        
+        //launchConacts();
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view_in_contacts);
         mRecyclerView.setHasFixedSize(true);
@@ -122,13 +127,13 @@ public class ContactsActivity extends AppCompatActivity implements ContactsAdapt
         mRecyclerView.setLayoutManager(mLayoutManager);
         mAdapter = new ContactsAdapter(contactsArrayList, ContactsActivity.this);
         mRecyclerView.setAdapter(mAdapter);
-        if(contactsArrayList.size()==0){
+        if (contactsArrayList.size() == 0) {
             noItem.setVisibility(View.VISIBLE);
         }
         for (int i = 0; i < contactsArrayList.size(); i++) {
 
             Log.e("name:", "" + contactsArrayList.get(i).getName());
-            for (int j = 0;j<contactsArrayList.get(i).getMobileNumber().size();j++) {
+            for (int j = 0; j < contactsArrayList.get(i).getMobileNumber().size(); j++) {
                 Log.e("num:", contactsArrayList.get(i).getMobileNumber().get(j));
             }
         }
@@ -144,23 +149,21 @@ public class ContactsActivity extends AppCompatActivity implements ContactsAdapt
                     Contacts singleContact = contactsArrayList.get(i);
                     if (contactsArrayList.get(i).getFlag()) {
 
-                        data = data + "\n" + singleContact.getName().toString()+"    "+singleContact.getSelectedMobileNumber();
+                        data = data + "\n" + singleContact.getName().toString() + "    " + singleContact.getSelectedMobileNumber();
                         counter++;
-                        mEditor.putBoolean("checkbox_"+contactsArrayList.get(i).getName(),true);
-                        mEditor.putString("selected_mobile_number_for_"+contactsArrayList.get(i).getName(),""+singleContact.getSelectedMobileNumber());
-                    }
-                    else
-                    {
-                        mEditor.putBoolean("checkbox_"+contactsArrayList.get(i).getName(),false);
-                        mEditor.putString("selected_mobile_number_for_"+contactsArrayList.get(i).getName(),""+singleContact.getSelectedMobileNumber());
+                        mEditor.putBoolean("checkbox_" + contactsArrayList.get(i).getName(), true);
+                        mEditor.putString("selected_mobile_number_for_" + contactsArrayList.get(i).getName(), "" + singleContact.getSelectedMobileNumber());
+                    } else {
+                        mEditor.putBoolean("checkbox_" + contactsArrayList.get(i).getName(), false);
+                        mEditor.putString("selected_mobile_number_for_" + contactsArrayList.get(i).getName(), "" + singleContact.getSelectedMobileNumber());
                     }
 
                 }
                 mEditor.commit();
                 Toast.makeText(ContactsActivity.this, "Selected Students: \n" + data, Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(ContactsActivity.this,ReportActivity.class);
-                intent.putExtra("TOTAL_KEY",contactsArrayList.size()+"");
-                intent.putExtra("SELECTED_KEY",counter+"");
+                Intent intent = new Intent(ContactsActivity.this, ReportActivity.class);
+                intent.putExtra("TOTAL_KEY", contactsArrayList.size() + "");
+                intent.putExtra("SELECTED_KEY", counter + "");
                 startActivity(intent);
 
             }
@@ -170,49 +173,46 @@ public class ContactsActivity extends AppCompatActivity implements ContactsAdapt
     @Override
     public void onListItemClick(final int clickedItemIndex, int whichClick) {
 
-        switch (whichClick){
-            case CHECK_CLICK:{
+        switch (whichClick) {
+            case CHECK_CLICK: {
                 //Toast.makeText(ContactsActivity.this, "Clicked on Checkbox: "+clickedItemIndex , Toast.LENGTH_SHORT).show();
-               if(contactsArrayList.get(clickedItemIndex).getMobileNumber().size()>1) {
-                   final String items[] = new String[contactsArrayList.get(clickedItemIndex).getMobileNumber().size()] ;
-                   for (int j = 0;j<contactsArrayList.get(clickedItemIndex).getMobileNumber().size();j++) {
-                       items[j] = contactsArrayList.get(clickedItemIndex).getMobileNumber().get(j);
-                   }
+                if (contactsArrayList.get(clickedItemIndex).getMobileNumber().size() > 1) {
+                    final String items[] = new String[contactsArrayList.get(clickedItemIndex).getMobileNumber().size()];
+                    for (int j = 0; j < contactsArrayList.get(clickedItemIndex).getMobileNumber().size(); j++) {
+                        items[j] = contactsArrayList.get(clickedItemIndex).getMobileNumber().get(j);
+                    }
 
 
+                    AlertDialog levelDialog;
 
 
-                   AlertDialog levelDialog;
+                    // Creating and Building the Dialog
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Please select the mobile number");
+                    builder.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int item) {
 
 
+                            mobileNumberSelected = items[item];
+                            contactsArrayList.get(clickedItemIndex).setSelectedMobileNumber(mobileNumberSelected);
+                            // levelDialog.dismiss();
+                        }
+                    });
+                    builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface arg0, int arg1) {
+                            // Toast.makeText(ContactsActivity.this, "You clicked yes button", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    levelDialog = builder.create();
+                    levelDialog.show();
 
-                   // Creating and Building the Dialog
-                   AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                   builder.setTitle("Please select the mobile number");
-                   builder.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
-                       public void onClick(DialogInterface dialog, int item) {
-
-
-                           mobileNumberSelected = items[item];
-                           contactsArrayList.get(clickedItemIndex).setSelectedMobileNumber(mobileNumberSelected);
-                          // levelDialog.dismiss();
-                       }
-                   });
-                   builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
-                       @Override
-                       public void onClick(DialogInterface arg0, int arg1) {
-                          // Toast.makeText(ContactsActivity.this, "You clicked yes button", Toast.LENGTH_LONG).show();
-                       }
-                   });
-                   levelDialog = builder.create();
-                   levelDialog.show();
-
-               }
+                }
 
                 break;
             }
         }
-        
+
     }
 
     protected void onSaveInstanceState(Bundle state) {
@@ -223,24 +223,98 @@ public class ContactsActivity extends AppCompatActivity implements ContactsAdapt
         state.putParcelable(LIST_STATE_KEY, mListState);
     }
 
-  
 
     protected void onRestoreInstanceState(Bundle state) {
         super.onRestoreInstanceState(state);
 
         // Retrieve list state and list/item positions
-        if(state != null)
+        if (state != null)
             mListState = state.getParcelable(LIST_STATE_KEY);
     }
 
-   
 
     @Override
     protected void onResume() {
         super.onResume();
 
+
+//        if (!mCalledFromOncreate) {
+//            contactsArrayList.clear();
+//            launchConacts();
+//
+//            mAdapter.notifyDataSetChanged();
+//
+//        }
+
+
         if (mListState != null) {
             mLayoutManager.onRestoreInstanceState(mListState);
         }
+    }
+
+    void launchConacts() {
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " COLLATE NOCASE ASC");
+
+        while (phones.moveToNext()) {
+
+
+            String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumberStr = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            try {
+
+
+                final PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
+                PhoneNumber phoneNumber = phoneNumberUtil.parse(phoneNumberStr, Locale.getDefault().getCountry());
+                PhoneNumberUtil.PhoneNumberType phoneNumberType = phoneNumberUtil.getNumberType(phoneNumber);
+
+
+                if (phoneNumberType == PhoneNumberType.MOBILE) {
+
+                    if (name.equals(contactName)) {
+
+
+                        phoneNumberStr = phoneNumberStr.replaceAll(" ", "");
+
+                        if (phoneNumberStr.contains(mobileNumber)) {
+                        } else {
+                            mobileNumber = String.valueOf(phoneNumber.getNationalNumber());
+                            Log.e("phone: ", " " + phoneNumber);
+                            contactsObject.setMobileNumber(mobileNumber);
+
+                        }
+
+                    } else {
+
+                        if (contactsObject != null) {
+                            contactsArrayList.add(contactsObject);
+                        }
+                        contactsObject = new Contacts();
+
+                        contactName = name;
+                        mobileNumber = String.valueOf(phoneNumber.getNationalNumber());
+                        Log.e("name: ", " " + name);
+                        Log.e("phone: ", " " + mobileNumber);
+
+                        contactsObject.setName(name);
+                        contactsObject.setMobileNumber(mobileNumber);
+                        contactsObject.setFlag(mSharedPreferences.getBoolean("checkbox_" + name, false));
+                        contactsObject.setSelectedMobileNumber(mSharedPreferences.getString("selected_mobile_number_for_" + name, mobileNumber));
+                    }
+
+                }
+            } catch (Exception e) {
+            }
+            if (phones.isLast()) {
+                contactsArrayList.add(contactsObject);
+            }
+
+        }
+        phones.close();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mCalledFromOncreate = false;
     }
 }
